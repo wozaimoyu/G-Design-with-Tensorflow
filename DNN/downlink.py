de83f4562dc66876e7116320fb56539cef8e6c43
Walker-rx <1449552959@qq.com>
2021-05-04 23:51:25 +0800

    far from finish

--- DNN/downlink.py
+++ DNN/downlink.py
@@ -6,70 +6,102 @@ import numpy as np
 import hk
 import os
 import math
-global M,K,P,seta
-M=64
-K=4
-P=1
-seta=0.1
+global g_M,g_K,g_P,g_seta
+g_M=64
+g_K=4
+g_P=1
+g_seta=0.1
     
-
 def Wrf(K,La,M):
-    wrf=[[np.exp(complex(0,np.random.uniform(1.0,360.0))) for i in range(1,M+1)] for j in range(1,K*La+1)]
-    return wrf
-def nk(M):
-    nk=[[complex(np.random.normal(loc=0.0,scale=0.1),np.random.normal(loc=0.0,scale=0.1))] for i in range(1,M+1)]
-    return nk
-def cre_yk(M,num,hk):
-    yk=np.array([],dtype=complex)
-    for i in range(num):
-        tem_nk=nk(M)
-        print(i)
-        yk=np.append(yk,np.matmul(Wrf(4,6,M),hk+tem_nk)) 
-    return yk
-user_hk=hk.h_k()
-yk=cre_yk(M,100000,user_hk)
-out=yk
+    wrf_real=np.array([])
+    wrf_imag=np.array([])
+    for x in range(K*La):
+        wrf_real_tep=np.array([])
+        wrf_imag_tep=np.array([])
+        for i in range(M):
+            aod=np.random.uniform(1.0,360.0)
+            wrf_real_tep=np.append(wrf_real_tep,np.cos(aod))
+            wrf_imag_tep=np.append(wrf_imag_tep,np.sin(aod))
+        # print('real_tep=',wrf_real_tep)
+        # print('imag_tep=',wrf_imag_tep)
 
-out=layers.Dense(2048,activation=tf.nn.relu)(out)
+        if x!=0:
+            wrf_real_tep=np.mat(wrf_real_tep)
+            wrf_imag_tep=np.mat(wrf_imag_tep)
+
+        wrf_real=np.r_[wrf_real,wrf_real_tep]
+        wrf_imag=np.r_[wrf_imag,wrf_imag_tep]
+        
+        if x==0:
+            wrf_real=np.mat(wrf_real)
+            wrf_imag=np.mat(wrf_imag)
+    return wrf_real,wrf_imag
+
+def nk(M):
+    nk_real=np.array([])
+    nk_imag=np.array([])
+    for i in range(M):
+        nk_real=np.append(nk_real,np.random.normal(loc=0.0,scale=0.1))
+        nk_imag=np.append(nk_imag,np.random.normal(loc=0.0,scale=0.1))
+    nk_real=np.mat(nk_real).T
+    nk_imag=np.mat(nk_imag).T
+    return nk_real,nk_imag
+
+def cre_yk(M,num,yhk_real,yhk_imag):
+    Wrf_real,Wrf_imag=Wrf(4,6,64)
+    tem_nk_real,tem_nk_imag=nk(64)
+    yk_real=np.matmul(Wrf_real,yhk_real+tem_nk_real)-np.matmul(Wrf_imag,yhk_imag+tem_nk_imag)
+    yk_imag=np.matmul(Wrf_real,yhk_imag+tem_nk_imag)+np.matmul(Wrf_imag,yhk_real+tem_nk_real)
+    yk_real=yk_real.T
+    yk_imag=yk_imag.T
+    #print(yk_real,yk_imag)
+    for i in range(num):
+        Wrf_real,Wrf_imag=Wrf(4,6,64)
+        tem_nk_real,tem_nk_imag=nk(64)
+        # print(i)
+        yk_real_tep=np.matmul(Wrf_real,yhk_real+tem_nk_real)-np.matmul(Wrf_imag,yhk_imag+tem_nk_imag)
+        yk_imag_tep=np.matmul(Wrf_real,yhk_imag+tem_nk_imag)+np.matmul(Wrf_imag,yhk_real+tem_nk_real)
+        yk_real_tep=yk_real_tep.T
+        yk_imag_tep=yk_imag_tep.T
+        #print(yk_real_tep,yk_imag_tep)
+        yk_real=np.c_[yk_real,yk_real_tep]
+        yk_imag=np.c_[yk_imag,yk_imag_tep]
+        #print('this time:',yk_real,yk_imag)
+    return yk_real,yk_imag
+
+user_hk_real,user_hk_imag=hk.cre_hk_real(6,64)
+yk_real,yk_imag=cre_yk(64,4,user_hk_real,user_hk_imag) #样本数=num+1
+yk_real=tf.convert_to_tensor(yk_real,dtype=tf.float32)
+yk_imag=tf.convert_to_tensor(yk_imag,dtype=tf.float32)
+# print('yk=',yk_real,'user_imag=',yk_imag)
+
+ykreal=tf.keras.Input(shape=(120,),dtype=tf.float32)
+ykimag=tf.keras.Input(shape=(120,),dtype=tf.float32)
+
+out=layers.Dense(2048,activation=tf.nn.relu)(ykreal)
 out=layers.Dense(1024,activation=tf.nn.relu)(out)
 out=layers.Dense(512,activation=tf.nn.relu)(out)
 out=layers.Dense(64,activation=None)(out)
-out=tf.math.exp(out)
 
-model=keras.Model(yk,out)
-loss=-1*tf.math.log(1+P*math.pow(np.abs(tf.matmul(user_hk.H,out)),2)/(M*K*seta*seta))/tf.math.log(2.0)
-model.add_loss(loss)
-model.compile( optimizer = tf.keras.optimizers.SGD(lr = 0.001))
-model.fit(yk,batch_size=1000,epochs=50)
+concat = keras.layers.concatenate([ykimag,out])
+
+output = keras.layers.Dense(1)(concat)
+output2=output2 = keras.layers.Dense(1)(out)
+
+model = keras.models.Model(inputs = [ykreal,ykimag],outputs = [output,output2])
+# out=tf.math.exp(out)
+model.compile(loss="mean_squared_error", optimizer="adam")
+model.fit([yk_real,yk_imag], batch_size=120,epochs=10)
 
 
+# model=keras.Model(yk_real,out)
+# model2=keras.Model(ykimag,out)
+# loss=-1*tf.math.log(1+P*math.pow(np.abs(tf.matmul(user_hk_real.H,out)),2)/(M*K*seta*seta))/tf.math.log(2.0)
+# model.add_loss(loss)
+# model.compile( optimizer = tf.keras.optimizers.SGD(lr = 0.001))
+# model.fit(yk,batch_size=1000,epochs=50)
 
 
-# class MyLayer(layers.Layer):
-#     def __init__(self, in_dim, out_dim):
-#         super(MyLayer, self).__init__()
-#         self.kernel = self.add_variable("w",(in_dim, out_dim),trainable=True)
-#         self.bias = self.add_variable("b",(out_dim,1),trainable=True)
-#     def call(self, input, training=None):
-#         output = tf.matmul(input, self.kernel) + self.bias
-#         return output
-
-# class MyModel(keras.Model):
-#     def __init__(self):
-#         super(MyModel, self).__init__()
-#         # self.layer_1 = MyLayer(Nrf*La,2048)
-#         self.layer_2 = MyLayer(2048,1024)
-#         self.layer_3 = MyLayer(1024,512)
-#         self.layer_4 = MyLayer(512,64)
-#     def call(self, x_train, training=None):
-#         out=self.layer_1(inputs)
-#         out=tf.nn.relu(out)
-#         out=self.layer_2(out)
-#         out=tf.nn.relu(out)
-#         out=self.layer_3(out)
-#         out=tf.nn.relu(out)
-#         out=self.layer_4(out)
-#         return out
 
 
 
--- DNN/hk.py
+++ DNN/hk.py
@@ -7,103 +7,33 @@ import math
 import random
 
 
-def h_k(lp=6,M=64):
-    h_k=np.zeros(shape=(1,M),dtype=complex)
+def cre_hk_real(lp,M):
+    hk_real=np.zeros(shape=(1,M))
+    hk_imag=np.zeros(shape=(1,M))
     for i in range(lp):
-        at=np.array([],dtype=complex)
+        at_real=np.array([])
+        at_imag=np.array([])
         for x in range(M):
-            aod=random.uniform(1.0,90.0)
-            at=np.append(at,np.exp(complex(0,np.pi*x*np.sin(aod))))          #天线距离为0.5倍波长
+            aod=np.sin(np.random.uniform(1.0,90.0))
+            at_real=np.append(at_real,np.cos(np.pi*x*aod)) 
+            at_imag=np.append(at_imag,np.sin(np.pi*x*aod))         #天线距离为0.5倍波长
             # print(i,x,aod,at)
-        alpha=complex(np.random.normal(loc=0.0,scale=1.0),np.random.normal(loc=0.0,scale=1.0))
-        h_k=h_k+1/np.sqrt(lp)*alpha*at
+        alpha_real=np.random.normal(loc=0.0,scale=1.0)
+        alpha_imag=np.random.normal(loc=0.0,scale=1.0)
+        hk_real=hk_real+1/np.sqrt(lp)*alpha_real*at_real-1/np.sqrt(lp)*alpha_imag*at_imag
+        hk_imag=hk_imag+1/np.sqrt(lp)*alpha_real*at_imag+1/np.sqrt(lp)*alpha_imag*at_real
+        hk_real=np.mat(hk_real)
+        hk_imag=np.mat(hk_imag)
+    # hk_real=tf.convert_to_tensor(hk_real.T,dtype=tf.float32)
+    # hk_imag=tf.convert_to_tensor(hk_imag.T,dtype=tf.float32)
+    # print('hk_real=',hk_real)
+    # print('hk_imag=',hk_imag)
     # h_k=np.mat(hk)
         # print("hk等于",h_k)
-    return h_k.T
-# b=h_k()
-# print("b等于",b)
+    return hk_real.T,hk_imag.T
+# a,b=cre_hk_real(6,6)
+# print('a=',a)
+# print('b=',b)
+
 
     
-# mnist = keras.datasets.mnist
-
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
-# x_train, x_test = x_train / 255.0, x_test / 255.0
-
-# x_train = x_train[..., tf.newaxis]
-# x_test = x_test[..., tf.newaxis]
-
-# train_ds = tf.data.Dataset.from_tensor_slices(
-#     (x_train, y_train)).shuffle(10000).batch(32)
-# test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
-
-# class MyModel(Model):
-#     def __init__(self):
-#         super(MyModel, self).__init__()
-#         self.conv1 = Conv2D(32, 3, activation='relu')
-#         self.flatten = Flatten()
-#         self.d1 = Dense(128, activation='relu')
-#         self.d2 = Dense(10, activation='softmax')
-
-#     def call(self, x):
-#         x = self.conv1(x)
-#         x = self.flatten(x)
-#         x = self.d1(x)
-#         return self.d2(x)
-
-
-# model = MyModel()
-
-# loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
-
-# optimizer = tf.keras.optimizers.Adam()
-
-# train_loss = tf.keras.metrics.Mean(name='train_loss')
-# train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
-#     name='train_accuracy')
-
-# test_loss = tf.keras.metrics.Mean(name='test_loss')
-# test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
-#     name='test_accuracy')
-
-
-# @tf.function
-# def train_step(images, labels):
-#     with tf.GradientTape() as tape:
-#         predictions = model(images)
-#         loss = loss_object(labels, predictions)
-#     gradients = tape.gradient(loss, model.trainable_variables)
-#     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-
-#     train_loss(loss)
-#     train_accuracy(labels, predictions)
-
-
-# @tf.function
-# def test_step(images, labels):
-#     predictions = model(images)
-#     t_loss = loss_object(labels, predictions)
-
-#     test_loss(t_loss)
-#     test_accuracy(labels, predictions)
-
-
-# EPOCHS = 5
-
-# for epoch in range(EPOCHS):
-#     train_loss.reset_states()
-#     train_accuracy.reset_states()
-#     test_loss.reset_states()
-#     test_accuracy.reset_states()
-
-#     for images, labels in train_ds:
-#         train_step(images, labels)
-
-#     for test_images, test_labels in test_ds:
-#         test_step(test_images, test_labels)
-
-#     template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
-#     print(template.format(epoch+1,
-#                           train_loss.result(),
-#                           train_accuracy.result()*100,
-#                           test_loss.result(),
-#                           test_accuracy.result()*100))
